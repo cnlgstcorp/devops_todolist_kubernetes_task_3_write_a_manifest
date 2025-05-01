f454ca3b495466e2f6e48ceec3ee08e7d320020e
Ioann Nadtocheev <ioannnatocheev@gmail.com>
2025-05-01 17:58:06 +0300

    KUBE

--- src/accounts/views.py
+++ src/accounts/views.py
@@ -1,6 +1,7 @@
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.models import User
 from django.shortcuts import redirect, render
+from django.http import JsonResponse
 
 from accounts.forms import LoginForm, RegistrationForm
 from lists.forms import TodoForm
--- src/api/urls.py
+++ src/api/urls.py
@@ -2,6 +2,7 @@ from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
 from api import views
+from . import views
 
 router = DefaultRouter()
 router.register(r"users", views.UserViewSet)
@@ -10,5 +11,7 @@ router.register(r"todos", views.TodoViewSet)
 
 app_name = "api"
 urlpatterns = [
-    path("", include(router.urls))
+    path("", include(router.urls)),
+    path('health/ready/', views.readiness, name='readiness'),
+    path('health/live/', views.liveness, name='liveness'),
 ]
