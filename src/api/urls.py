f3fa990affc4f9b5b06f3dc2f085c7289697b9d7
Ioann Nadtocheev <ioannnatocheev@gmail.com>
2025-05-01 18:05:35 +0300

    KUBE

--- src/api/urls.py
+++ src/api/urls.py
@@ -2,7 +2,6 @@ from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
 from api import views
-from . import views
 
 router = DefaultRouter()
 router.register(r"users", views.UserViewSet)
